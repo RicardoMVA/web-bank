eb57a96eb52d689b1eb884546bbf255929174b1f
RicardoMVA <ricardovalenca@gmail.com>
2020-01-21 15:04:10 -0300

    create 'account' model class; uncomment some 'customer' functionality

--- src/main/java/com/br/qualiti/bank/model/Customer.java
+++ src/main/java/com/br/qualiti/bank/model/Customer.java
@@ -12,29 +12,37 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 /*
-* A anotação @Entity pertence ao JPA e isso significa que a classe será automaticamente mapeada à tabela com o mesmo nome
-* (classe Account e tabela Account).
+* A anotação @Entity pertence ao JPA e isso significa que a classe será automaticamente mapeada à tabela com o mesmo 
+* nome (classe Customer e tabela Customer).
 * Todos os atributos dessa classe também serão mapeados com as respectivas colunas.
-* Podemos omitir a anotação @Column para cada atributo da classe desde que o nome do atributo seja o mesmo nome da coluna.
-* Caso a coluna tenha o nome diferente do atributo precisamos especificar.
+* Podemos omitir a anotação @Column para cada atributo da classe desde que o nome do atributo seja o mesmo nome da 
+* coluna. Caso a coluna tenha o nome diferente do atributo precisamos especificar.
 */
 @Entity
 public class Customer {
 
 	@Id
+//	isso faz a coluna 'id' ser a chave primária
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
-
+	
+//	aqui definimos alguns parâmetros que serão usados pelo banco de dados
 	@Column(length = 20, nullable = false, unique = true)
 	private String CPF;
 
-	/*
-	 * @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy =
-	 * "customer") private List<Account> accounts;
-	 */
+//	aqui definimos que a tabela 'customer' vai ter uma associação de 1 para N com a lista 'accounts'
+//	cascade type all significa que todas as contas serão deletadas quando o cliente for deletado
+//	FetchType.LAZY: It fetches the child entities lazily, that is, at the time of fetching parent entity it 
+//	just fetches proxy (created by cglib or any other utility) of the child entities and when you access
+//	any property of child entity then it is actually fetched by hibernate.
+//	FetchType.EAGER: it fetches the child entities along with parent.
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "customer")
+	private List<Account> accounts;
 
+//	construtor vazio, getters e setters devem ser definidos
+//	isso é um requisito do sping, o professor não explicou o porque
 	public Customer() {
 	}
 
@@ -62,10 +70,12 @@ public class Customer {
 		CPF = cPF;
 	}
 
-	/*
-	 * public List<Account> getAccounts() { return accounts; }
-	 * 
-	 * public void setAccounts(List<Account> accounts) { this.accounts = accounts; }
-	 */
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}
 
 }
